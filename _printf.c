ba5cf9a08ff8015777fb874128063ba06446af33
zoubjd <zouhairbajdouri19@gmail.com>
2024-02-03 01:54:46 +0100

    still trying with i

--- _printf.c
+++ _printf.c
@@ -78,52 +78,56 @@ switch (*(++ptr))
 	case 'i':
 {
 	int n = va_arg(args, int);
+    int temp = n;
+    int num_chars = 0;
+    char *num_str;
+    int i;
 
-	
     if (n < 0)
     {
-    _putchar('-');
-    count++;
-    n = -n;
+        _putchar('-');
+        count++;
+        n = -n;
+        temp = n;
     }
-	if (n == 0)
+
+    if (n == 0)
     {
-    _putchar('0');
-    count++;
+        _putchar('0');
+        count++;
     }
     else
     {
-    int temp = n;
-    int num_chars = 0;
-	char *num_str;
-	int i;
+        while (temp != 0)
+        {
+            temp /= 10;
+            num_chars++;
+        }
 
-    while (temp != 0)
-    {
-	temp /= 10;
-	num_chars++;
-	}
-	num_str = malloc(num_chars + 1);
-	if (num_str == NULL)
-	{	
-    return -1;
-	}
+        num_str = malloc(num_chars + 1);
+        if (num_str == NULL)
+        {
+            return -1;
+        }
 
-	num_str[num_chars] = '\0';
-	while (n != 0)
-{
-    --num_chars;
-    num_str[num_chars] = (char)((n % 10) + '0');
-    n /= 10;
-}
+        num_str[num_chars] = '\0';
+        temp = n; // Reset temp to the original value of n
+        for (i = num_chars - 1; i >= 0; i--)
+        {
+            num_str[i] = (char)((temp % 10) + '0');
+            temp /= 10;
+        }
 
-for (i = 0; i < num_chars; i++)
-{
-    _putchar(num_str[i]);
-    count++;
-}
-free(num_str);			
-}
+        for (i = 0; i < num_chars; i++)
+        {
+            _putchar(num_str[i]);
+            count++;
+        }
+
+        free(num_str);
+    }
+    break;
+
 	break;
 }
 
